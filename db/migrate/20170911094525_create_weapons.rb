60b097e907b76b9fa1b210c496aac155a680351b
Yuka Kato <satoimo.to.imo@gmail.com>
2017-09-11 18:49:11 +0900

    Generate weapon model and add association, then migrate database

---
+++ db/migrate/20170911094525_create_weapons.rb
@@ -0,0 +1,9 @@
+class CreateWeapons < ActiveRecord::Migration[5.1]
+  def change
+    create_table :weapons do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911093423) do
+ActiveRecord::Schema.define(version: 20170911094525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20170911093423) do
     t.index ["lft"], name: "index_categories_on_lft"
     t.index ["parent_id"], name: "index_categories_on_parent_id"
     t.index ["rgt"], name: "index_categories_on_rgt"
+  end
+
+  create_table "weapons", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
