fd6aeae40dace2d4af0c145f6232def7d3fde9f0
Yuka Kato <satoimo.to.imo@gmail.com>
2017-09-12 11:23:21 +0900

    Create script to generate test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.delete_all
+Weapon.delete_all
+
+puts "Generating records of categories and weapons..."
+
+CATEGORY_A = {
+  category: "CATEGORY_A",
+  weapons_with_categories: [
+    {category: "Roller", weapons: ["Carbon Roller", "Splat Roller", "Dynamo Roller", "Flingza Roller", "Hero Roller Replica"]},
+    {category: "Shooter", weapons: ["Splat Shooter", "Aerospray MG"]},
+    {category: "Charger", weapons: ["Splat Charger"]}
+  ]
+}
+
+CATEGORY_B = {
+  category: "CATEGORY_B",
+  weapons_with_categories: [
+    {category: "Dualies", weapons: ["Splat Dualies"]},
+    {category: "Splatling", weapons: ["Heavy Splatling"]}
+  ]
+}
+
+CATEGORY_C = {
+  category: "CATEGORY_C",
+  weapons_with_categories: [
+    {category: "Brella", weapons: ["Splat Brella"]},
+    {category: "Slosher", weapons: ["Tri-Slosher"]}
+  ]
+}
+
+[CATEGORY_A, CATEGORY_B, CATEGORY_C].each do |cate|
+  ActiveRecord::Base.transaction do
+    root_category = Category.create!(name: cate[:category])
+
+    cate[:weapons_with_categories].each do |weapons_with_category|
+      child_category = root_category.children.create!(name: weapons_with_category[:category])
+
+      weapons_with_category[:weapons].each do |weapon|
+        child_category.weapons.create!(name: weapon)
+      end
+    end
+  end
+end
+
+puts "Done."
